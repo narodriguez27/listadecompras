ee8544a59906bc60e1bc4aba9e251e3caefac527
Nicolas <narodriguez@outlook.com>
2022-03-15 20:48:37 -0300

    Se agrega funcionalidades y css

--- L/app.js
+++ L/app.js
@@ -1,29 +1,48 @@
 let btnEnviar = document.getElementById ("btnEnviar");
+let btnClear = document.getElementById("bntClear");
 let bodyEntero = document.querySelector("body");
+let input = document.createElement("input");
+let ulList = document.getElementById("ulList");
 let textValue = document.getElementById("textValue");
-let newArr = [];
-btnEnviar.onclick =  (click) => {
-    let textContainer = textValue.value;
-    newArr.push(textContainer);
-    console.log(newArr);
-    createDivs();
+let propertyLi = document.getElementById ("propertyLi");
+let propertyCheck = document.getElementById ("propertyCheck");
+let propertyLabel = document.getElementById ("propertyLabel");
+
+btnEnviar.onclick =  () => {
+   
+        if (!textValue.value == " "){
+            newElements();
+          
+    }else{ 
+        alert("Ingresar un producto");
+    }   
+}
+btnClear.onclick = () => {
+        remove();
 }
 
-let createDivs = () => {
-   let createDiv = document.createElement("div");
-   let createUl = document.createElement("ul");
-   let createLi = document.createElement("li");
-   let input = document.createElement("input");
-   input.type = "checkbox";
-   let textInformate = document.createElement("p");
-   bodyEntero.appendChild(createDiv); 
-   createDiv.appendChild(createUl);
-   createUl.appendChild(createLi);
-   createLi.appendChild(input);
-   input.appendChild(textInformate);
+let newElements = () => {
+    let createLi = document.createElement("li");
+    let createCheck = document.createElement("input");
+    let createLabel = document.createElement("label");
+    createLi.id = "propertyLi";
+    createLi.classList ="LiStyle"
+    createCheck.type = "checkbox";
+    createCheck.id = "propertyCheck";
+    createCheck.classList =" checkStyle";
+    createLabel.id = "propertyLabel";
+    createLabel.classList = "labelStyle";
+    createLabel.textContent = textValue.value;
+    createLi.appendChild(createCheck);
+    createLi.appendChild(createLabel);
+    ulList.appendChild(createLi);
 }
 
-
-
-
+let remove = () =>{
+        let lis = ulList.childNodes.length;
+        console.log(lis);
+        for (let i = 0; i <= lis; i++ ){
+            ulList.removeChild(ulList.firstChild);
+        }
+}
 
