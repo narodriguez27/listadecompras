2314efbe7b811aeb0547790480a3e25a11c2d461
Nicolas <narodriguez@outlook.com>
2022-03-28 13:48:21 -0300

    se hacen pruebas para validaciones sin exito

--- L/js/app.js
+++ L/js/app.js
@@ -5,12 +5,12 @@ let btnSendFood = document.getElementById ("btnSend");
 let ControlDivUl = document.getElementById ("ulDivControl");
 let foodList = document.getElementById ("ulListFood");
 let btnClearFood = document.getElementById ("btnClearList");
-let newArr = [];
+const newArr = [];
 //funciones:
 
 const createElementsList = () =>{
-  //SE CREAN ELElet contador = 1;MENTOS DINAMICOS DE LA LISTA DE ALIMENTOS
   let contador = 1;
+  //SE CREAN ELEMENTOS DINAMICOS DE LA LISTA DE ALIMENTOS
   let contectValue = contentText.value;
   //CREACION DE NODOS ELEMENTOS
   let listaFood = document.createElement('li');
@@ -23,12 +23,16 @@ const createElementsList = () =>{
   let spam = document.createElement('i');
   let spam2 = document.createElement('i');
   let spam3 = document.createElement('i');
+  let divControl = document.createElement('div');
+  divControl.className = "btnmov"
   
 
   //SE CREA ELEMENTO DE BOTON SUMA
   let suma = document.createElement('button');
-  listaFood.appendChild(suma);
+  listaFood.appendChild(divControl);
+  divControl.appendChild(suma);
   suma.className = `suma_${contectValue}`;
+  suma.className = "btnStyle_sumar"
   suma.appendChild(spam);
   spam.setAttribute("class", "fa-solid fa-plus");
   suma.addEventListener('click', () =>{
@@ -37,26 +41,26 @@ const createElementsList = () =>{
   })
  //SE CREA ELEMENTO DE BOTON RESTA
  let resta = document.createElement('button');
- listaFood.appendChild(resta);
+ listaFood.appendChild(divControl);
+ divControl.appendChild(resta);
  resta.className = `resta_${contectValue}`;
+ resta.className = "btnStyle_resta"
  resta.appendChild(spam2);
  spam2.setAttribute("class", "fa-solid fa-minus");
  resta.addEventListener('click', () =>{
   contadorRest();
  });
-//3 botones 1 agrega 1 decrementa 1 elimina
-//cada boton debe hacer incrementar decrementar y eliminar el items
+ 
 // SE CREA BOTON ELIMINAR
-
 let btnElimiar = document.createElement('button');
-listaFood.appendChild(btnElimiar);
-btnElimiar.className = 'btnEliminar';
+listaFood.appendChild(divControl);
+divControl.appendChild(btnElimiar);;
 btnElimiar.appendChild(spam3);
-spam3.setAttribute("class","fa-solid fa-trash");
+spam3.setAttribute("class","fa-solid fa-trash-can");
 btnElimiar.className = `eliminar_${contectValue}`;
-btnElimiar.addEventListener('click', () => {
-  let a = newArr.pop();
-  a.remove();
+btnElimiar.className = "btnStyle_eliminar"
+btnElimiar.addEventListener('click', (e) => {
+   
 })
 
 //FUNCIONALIDADES 
@@ -74,21 +78,21 @@ let contadorRest = () =>{
   contentLabel.textContent = `${contador} X/U ${contectValue}`;
   if (contador == 0){
     resta.disabled = true;
+     }
   }
-}
-}
 
+}
 let eliminar = () =>{
     for (let list of newArr) {
       list.remove();
-    }
+      
+  }
 }
+
 
 //eventos:
 btnSendFood.addEventListener('click',createElementsList);
 btnClearFood.addEventListener('click', eliminar);
-
-
 
 
 
